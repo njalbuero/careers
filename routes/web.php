7700b50715160aea5c734ff79c882f1fcdb69575
Nhielsan <njalbuero@gmail.com>
2021-06-21 20:02:02 +0800

    installed breeze

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\JobsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,12 +16,9 @@ use App\Http\Controllers\JobsController;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/backoffice/dashboard', [DashboardController::class, 'index']);
-Route::get('/backoffice/analytics/{department}', [DashboardController::class, 'analytics']);
-Route::get('/backoffice/listings/{department}', [JobsController::class, 'index']);
-Route::get('/backoffice/listings/{department}/create', [JobsController::class, 'create']);
-Route::post('/backoffice/listings/{department}', [JobsController::class, 'store']);
-Route::get('/backoffice/listings/{department}/{job}/view', [JobsController::class, 'view']);
-Route::get('/backoffice/listings/{department}/{job}/edit', [JobsController::class, 'edit']);
-Route::put('/backoffice/listings/{department}/{job}', [JobsController::class, 'update']);
-Route::delete('/backoffice/listings/{department}/{job}', [JobsController::class, 'destroy']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
