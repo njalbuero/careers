d1a141279def685c2b79c7dace14b213b693c860
Nhielsan <njalbuero@gmail.com>
2021-06-25 14:08:17 +0800

    changes

--- resources/views/backoffice/dashboard/index.blade.php
+++ resources/views/backoffice/dashboard/index.blade.php
@@ -27,52 +27,53 @@
                         </div>
                         <div class="card-body spur-card-body-chart">
                             @if ($jobs_total_count)
-                            <canvas id="spurChartjsDougnut"></canvas>
-                            <script>
-                                const jobs_count = [{!! json_encode($accounting_jobs_count) !!},
-                                    {!! json_encode($admin_jobs_count) !!},
-                                    {!! json_encode($graphics_jobs_count) !!},
-                                    {!! json_encode($mobile_jobs_count) !!},
-                                    {!! json_encode($qa_jobs_count) !!},
-                                    {!! json_encode($sales_jobs_count) !!},
-                                    {!! json_encode($web_jobs_count) !!}
-                                ];
-                                var ctx = document.getElementById("spurChartjsDougnut").getContext('2d');
-                                var myChart = new Chart(ctx, {
-                                    type: 'doughnut',
-                                    data: {
-                                        labels: ["Accounting", "Admin", "Graphics", "Mobile Development", "Quality Assurance", "Sales",
-                                            "Web Development"
-                                        ],
-                                        datasets: [{
-                                            label: 'Week',
-                                            data: jobs_count,
-                                            backgroundColor: [
-                                                '#1ABC9C',
-                                                '#2ECC71',
-                                                '#3498DB',
-                                                '#9B59B6',
-                                                '#F1C40F',
-                                                '#E67E22',
-                                                '#E74C3C',
+                                <canvas id="spurChartjsDougnut"></canvas>
+                                <script>
+                                    const jobs_count = [{!! json_encode($accounting_jobs_count) !!},
+                                        {!! json_encode($admin_jobs_count) !!},
+                                        {!! json_encode($graphics_jobs_count) !!},
+                                        {!! json_encode($mobile_jobs_count) !!},
+                                        {!! json_encode($qa_jobs_count) !!},
+                                        {!! json_encode($sales_jobs_count) !!},
+                                        {!! json_encode($web_jobs_count) !!}
+                                    ];
+                                    var ctx = document.getElementById("spurChartjsDougnut").getContext('2d');
+                                    var myChart = new Chart(ctx, {
+                                        type: 'doughnut',
+                                        data: {
+                                            labels: ["Accounting", "Admin", "Graphics", "Mobile Development", "Quality Assurance", "Sales",
+                                                "Web Development"
                                             ],
-                                            borderColor: '#fff',
-                                            fill: false
-                                        }]
-                                    },
-                                    options: {
-                                        legend: {
-                                            display: true
+                                            datasets: [{
+                                                label: 'Week',
+                                                data: jobs_count,
+                                                backgroundColor: [
+                                                    '#1ABC9C',
+                                                    '#2ECC71',
+                                                    '#3498DB',
+                                                    '#9B59B6',
+                                                    '#F1C40F',
+                                                    '#E67E22',
+                                                    '#E74C3C',
+                                                ],
+                                                borderColor: '#fff',
+                                                fill: false
+                                            }]
+                                        },
+                                        options: {
+                                            legend: {
+                                                display: true,
+                                                position: 'right'
+                                            }
                                         }
-                                    }
-                                });
-                            </script>
+                                    });
+                                </script>
                             @else
                                 <div class="text-center">
                                     No records
                                 </div>
                             @endif
-                            
+
                         </div>
                     </div>
                 </div>
@@ -103,57 +104,59 @@
                         </div>
                         <div class="card-body spur-card-body-chart">
                             @if ($applicants_total_count)
-                            <canvas id="spurChartjsTwoBars"></canvas>
-                            <script>
-                                const applicants_count = [{!! json_encode($accounting_applicants_count) !!},
-                                    {!! json_encode($admin_applicants_count) !!},
-                                    {!! json_encode($graphics_applicants_count) !!},
-                                    {!! json_encode($mobile_applicants_count) !!},
-                                    {!! json_encode($qa_applicants_count) !!},
-                                    {!! json_encode($sales_applicants_count) !!},
-                                    {!! json_encode($web_applicants_count) !!}
-                                ];
-                                var ctx = document.getElementById("spurChartjsTwoBars").getContext('2d');
-                                var myChart = new Chart(ctx, {
-                                    type: 'bar',
-                                    data: {
-                                        labels: ["Accounting", "Admin", "Graphics", "Mobile Development", "Quality Assurance", "Sales",
-                                            "Web Development"
-                                        ],
-                                        datasets: [{
-                                            label: 'Blue',
-                                            data: applicants_count,
-                                            backgroundColor: [
-                                                '#1ABC9C',
-                                                '#2ECC71',
-                                                '#3498DB',
-                                                '#9B59B6',
-                                                '#F1C40F',
-                                                '#E67E22',
-                                                '#E74C3C',
+                                <canvas id="spurChartjsTwoBars"></canvas>
+                                <script>
+                                    const applicants_count = [{!! json_encode($accounting_applicants_count) !!},
+                                        {!! json_encode($admin_applicants_count) !!},
+                                        {!! json_encode($graphics_applicants_count) !!},
+                                        {!! json_encode($mobile_applicants_count) !!},
+                                        {!! json_encode($qa_applicants_count) !!},
+                                        {!! json_encode($sales_applicants_count) !!},
+                                        {!! json_encode($web_applicants_count) !!}
+                                    ];
+                                    var ctx = document.getElementById("spurChartjsTwoBars").getContext('2d');
+                                    var myChart = new Chart(ctx, {
+                                        type: 'bar',
+                                        data: {
+                                            labels: ["Accounting", "Admin", "Graphics", "Mobile Development", "Quality Assurance", "Sales",
+                                                "Web Development"
                                             ],
-                                            borderColor: 'transparent'
-                                        }, ]
-                                    },
-                                    options: {
-                                        legend: {
-                                            display: false
+                                            datasets: [{
+                                                label: 'Blue',
+                                                data: applicants_count,
+                                                backgroundColor: [
+                                                    '#1ABC9C',
+                                                    '#2ECC71',
+                                                    '#3498DB',
+                                                    '#9B59B6',
+                                                    '#F1C40F',
+                                                    '#E67E22',
+                                                    '#E74C3C',
+                                                ],
+                                                borderColor: 'transparent'
+                                            }, ]
                                         },
-                                        scales: {
-                                            xAxes: [{
-                                                stacked: false,
-                                                beginAtZero: true,
-                                                ticks: {
-                                                    stepSize: 1,
-                                                    min: 0,
-                                                    autoSkip: false
-                                                }
-                                            }]
-                                        }
+                                        options: {
+                                            legend: {
+                                                display: false
+                                            },
+                                            scales: {
+                                                yAxes: [{
+                                                    ticks: {
+                                                        beginAtZero: true,
+                                                        suggestedMax: 5
+                                                    }
+                                                }],
+                                                xAxes: [{
+                                                    ticks: {
+                                                        autoSkip: false
+                                                    }
+                                                }]
+                                            }
 
-                                    }
-                                });
-                            </script>
+                                        }
+                                    });
+                                </script>
                             @else
                                 <div class="text-center">No Records</div>
                             @endif
